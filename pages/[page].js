809f592fc18b76e1115398fc26058a91b03256f6
Ivo Legzdins <ivo@Ivos-MacBook-Air.local>
2021-03-25 19:58:00 +0000

    first

---
+++ pages/[page].js
@@ -0,0 +1,69 @@
+import hydrate from "next-mdx-remote/hydrate";
+import renderToString from "next-mdx-remote/render-to-string";
+import Callout from "../components/callout";
+import { getSanityContent } from "../utils/sanity";
+
+export default function Page({ title, content }) {
+  const renderedContent = hydrate(content, {
+    components: {
+      Callout,
+    },
+  });
+
+  return (
+    <div>
+      <h1>{title}</h1>
+      {renderedContent}
+    </div>
+  );
+}
+
+export async function getStaticProps({ params }) {
+  const data = await getSanityContent({
+    query: `
+          query PageBySlug($slug: String!) {
+            allPage(where: { slug: { current: { eq: $slug } } }) {
+              title
+              content
+            }
+          }
+        `,
+    variables: {
+      slug: params.page,
+    },
+  });
+
+  const [pageData] = data.allPage;
+
+  const content = await renderToString(pageData.content, {
+    components: { Callout },
+  });
+
+  return {
+    props: {
+      title: pageData.title,
+      content,
+    },
+  };
+}
+
+export async function getStaticPaths() {
+  const data = await getSanityContent({
+    query: `
+          query AllPages {
+            allPage {
+              slug {
+                current
+              }
+            }
+          }
+        `,
+  });
+
+  const pages = data.allPage;
+
+  return {
+    paths: pages.map((p) => `/${p.slug.current}`),
+    fallback: false,
+  };
+}
